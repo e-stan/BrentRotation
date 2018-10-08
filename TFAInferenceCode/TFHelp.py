928c7f8e0fd8b17b02547e6191879dcf63a7288d
e_stan <estancl1234@gmail.com>
2018-10-08 10:46:49 -0500

    changed a few things in the jupyder notebook, rearranged the MP to work in general

--- TFAInferenceCode/TFHelp.py
+++ TFAInferenceCode/TFHelp.py
@@ -6,6 +6,94 @@ import copy
 from multiprocessing import Process, Queue
 import time
 
+def TFVAMP(CS, GE, TFAOriginal, columns, q, iterLimit=20):
+	"""
+		Perform the variable TFA analysis on entire TFA matrix with complete GE matrix. The results for the
+		range of possible values as well as the difference in factorization qualtiy are written to pickle files
+		with the mean TFA activity written to a csv
+	"""
+
+	TFAVariable = list(TFAOriginal)
+	TFVARange = [[[col, col] for col in row] for row in TFAOriginal]
+	TFVAQuality = [[[-1, -1] for col in row] for row in TFVARange]
+	for col in range(len(TFAOriginal[0])):
+		TFA = [[row[col]] for row in TFAOriginal]
+		singelGE = [[row[col]] for row in GE]
+		tfRange, meanTFActivities, algorithmResult = TFAHelper.TFVASingleCondition(CS, singelGE, TFA, iterLimit)
+
+		tempTFA = np.transpose(TFAVariable).tolist()
+		tempTFA[col] = meanTFActivities
+		TFAVariable = np.transpose(tempTFA).tolist()
+
+		tempTFVARange = np.transpose(TFVARange, (1, 0, 2)).tolist()
+		tempTFVARange[col] = tfRange
+		TFVARange = np.transpose(tempTFVARange, (1, 0, 2)).tolist()
+
+		tempTFVAQuality = np.transpose(TFVAQuality, (1, 0, 2)).tolist()
+		tempTFVAQuality[col] = algorithmResult
+		TFVAQuality = np.transpose(tempTFVAQuality, (1, 0, 2)).tolist()
+
+	q.put([columns, TFAVariable, TFVARange, TFVAQuality])
+
+
+def TFVAMPRun(CS, GE, TFAOriginal, iterLimit=20, write=True, outfile="TFVAResult", numCores=2):
+	colsPerProc = int(len(GE[0]) / numCores)
+	print(colsPerProc)
+	q = Queue()
+	startCol = 0
+	processes = []
+	TFAVariable = [[col for col in row] for row in TFAOriginal]
+	TFARange = [[[-1, -1] for col in row] for row in TFAOriginal]
+	TFAQuality = [[[-1, -1] for col in row] for row in TFAOriginal]
+
+	for p in range(numCores):
+		if p == numCores - 1:
+			tempGE = [row[startCol:] for row in GE]
+			tempTFA = [row[startCol:] for row in TFAOriginal]
+			endCol = -1
+		else:
+			endCol = startCol + colsPerProc
+			tempGE = [row[startCol:endCol] for row in GE]
+			tempTFA = [row[startCol:endCol] for row in TFAOriginal]
+		p = Process(target=TFVAMP, args=(CS, tempGE, tempTFA, [startCol, endCol], q))
+		p.start()
+		processes.append(p)
+		endCol = startCol
+	while any(x.is_alive() for x in processes): time.sleep(15)
+	while not q.empty():
+		[columns, TFAVar, TFVARan, TFVAQual] = q.get()
+		if columns[1] == -1:
+			TFAVariable = np.transpose(TFAVariable)
+			TFAVariable[columns[0]:] = np.transpose(TFAVar)
+			TFAVariable = np.transpose(TFAVariable).tolist()
+
+			TFARange = np.transpose(TFARange, (1, 0, 2)).tolist()
+			TFARange[columns[0]:] = np.transpose(TFVARan, (1, 0, 2))
+			TFARange = np.transpose(TFARange, (1, 0, 2)).tolist()
+
+			TFAQuality = np.transpose(TFAQuality, (1, 0, 2)).tolist()
+			TFAQuality[columns[0]:] = np.transpose(TFVAQual, (1, 0, 2))
+			TFAQuality = np.transpose(TFAQuality, (1, 0, 2)).tolist()
+		else:
+			TFAVariable = np.transpose(TFAVariable)
+			TFAVariable[columns[0]:columns[1]] = np.transpose(TFAVar)
+			TFAVariable = np.transpose(TFAVariable).tolist()
+
+			TFARange = np.transpose(TFARange, (1, 0, 2)).tolist()
+			TFARange[columns[0]:columns[1]] = np.transpose(TFVARan, (1, 0, 2))
+			TFARange = np.transpose(TFARange, (1, 0, 2)).tolist()
+
+			TFAQuality = np.transpose(TFAQuality, (1, 0, 2)).tolist()
+			TFAQuality[columns[0]:columns[1]] = np.transpose(TFVAQual, (1, 0, 2))
+			TFAQuality = np.transpose(TFAQuality, (1, 0, 2)).tolist()
+
+	if write:
+		TFAHelper.writeMatrix2csv(TFAVariable, open(outfile + ".csv", "w"))
+		pickle.dump(TFARange, open(outfile + "Range.pkl", "wb"))
+		pickle.dump(TFAQuality, open(outfile + "Quality.pkl", "wb"))
+
+	return TFAVariable, TFARange, TFAQuality
+
 
 class TFAHelper():
 
@@ -58,7 +146,7 @@ class TFAHelper():
 	  model.setObjective(obj,direction)
 
 	  # Solve
-	  model.setParam("OutputFlag",False)
+	  #model.setParam("OutputFlag",False)
 	  model.optimize()
 
 	  # Write model to a file
@@ -73,53 +161,53 @@ class TFAHelper():
 	    return False
 
 	@staticmethod
-	def TFVASingleCondition(CS,GE,TFAOriginal,iterLimit = 20):
+	def TFVASingleCondition(CS, GE, TFAOriginal, iterLimit=20):
 		"""
 			Perform TF activity variability analysis by sequentially maximimizing and minimizing
-			each TF's activity individually subjust to the contraint that the TF activity solves 
+			each TF's activity individually subjust to the contraint that the TF activity solves
 			the system with qualityCoefficient
 		"""
 
-		pGE = np.dot(CS,TFAOriginal).tolist() #determine GE vector current facotrization solves 
-		meanError = TFAHelper.standardError(pGE,GE) #compute error between current factorization and true expression
+		pGE = np.dot(CS, TFAOriginal).tolist()  # determine GE vector current facotrization solves
+		meanError = TFAHelper.standardError(pGE, GE)  # compute error between current factorization and true expression
 
-		lb = [0 for _ in range(len(CS[0]))]  #form lower bound for TF activity
-		lb[-1] = TFAOriginal[-1][0]-TFAOriginal[-1][0]/100 #fix pseudo TF activity 
-		ub = [GRB.INFINITY for _ in range(len(CS[0]))] #network is not bounded above 
-		ub[-1] = TFAOriginal[-1][0]+TFAOriginal[-1][0]/100 #fix upper bound for pseudo TF
+		lb = [0 for _ in range(len(CS[0]))]  # form lower bound for TF activity
+		lb[-1] = TFAOriginal[-1][0] - TFAOriginal[-1][0] / 100  # fix pseudo TF activity
+		ub = [GRB.INFINITY for _ in range(len(CS[0]))]  # network is not bounded above
+		ub[-1] = TFAOriginal[-1][0] + TFAOriginal[-1][0] / 100  # fix upper bound for pseudo TF
 
 		vtype = [GRB.CONTINUOUS for _ in range(len(CS[0]))]
-		c = [0 for _ in range(len( CS[0]))]  #vector to hold which TF is objective
-		tfRange = [[] for x in range(len(CS[0]))] #will hold the range of values for the TFAs
-		algorithmResult = [[] for x in range(len(CS[0]))] #will hold the relaxation of the current factorization result
+		c = [0 for _ in range(len(CS[0]))]  # vector to hold which TF is objective
+		tfRange = [[] for x in range(len(CS[0]))]  # will hold the range of values for the TFAs
+		algorithmResult = [[] for x in
+						   range(len(CS[0]))]  # will hold the relaxation of the current factorization result
 
 		for tf in range(len(CS[0])):
 			c[tf] = 1
-			for direction in [GRB.MINIMIZE,GRB.MAXIMIZE]:
+			for direction in [GRB.MINIMIZE, GRB.MAXIMIZE]:
 				qualityCoefficient = 0
 				success = False
 				iter = 0
-				while not success and iter < iterLimit: 
-					qualityCoefficient+=.1
+				while not success and iter < iterLimit:
+					qualityCoefficient += .1
 					sol = [-1 for _ in range(len(CS[0]))]
 					rhsUB = [qualityCoefficient for _ in range(len(CS))]
-					success = TFAHelper.dense_optimize(c,CS,pGE,rhsUB,lb,ub,vtype,sol,direction)
-					iter+=1
-				if success: 
+					success = TFAHelper.dense_optimize(c, CS, pGE, rhsUB, lb, ub, vtype, sol, direction)
+					iter += 1
+				if success:
 					tfRange[tf].append(sol[tf])
-					temp = np.dot(CS,TFAHelper.rowv2colv(sol)).tolist()
-					tempError = TFAHelper.standardError(temp,GE)
-					algorithmResult[tf].append(tempError-meanError)
+					temp = np.dot(CS, TFAHelper.rowv2colv(sol)).tolist()
+					tempError = TFAHelper.standardError(temp, GE)
+					algorithmResult[tf].append(tempError - meanError)
 				else:
 					tfRange[tf].append(TFAOriginal[tf][0])
 					algorithmResult[tf].append(-1)
 			c[tf] = 0
 
 		meanTFActivities = [np.mean(tf) for tf in tfRange]
-		return tfRange,meanTFActivities,algorithmResult
-
+		return tfRange, meanTFActivities, algorithmResult
 	@staticmethod
-	def TFVA(CS,GE,TFAOriginal,iterLimit=20,write=True,outfile = "TFVAResult"):
+	def TFVA(CS, GE, TFAOriginal, iterLimit=20, write=True, outfile="TFVAResult"):
 		"""
 			Perform the variable TFA analysis on entire TFA matrix with complete GE matrix. The results for the
 			range of possible values as well as the difference in factorization qualtiy are written to pickle files
@@ -127,127 +215,32 @@ class TFAHelper():
 		"""
 
 		TFAVariable = list(TFAOriginal)
-		TFVARange = [[[col,col] for col in row] for row in TFAOriginal]
-		TFVAQuality = [[[-1,-1] for col in row] for row in TFVARange]
+		TFVARange = [[[col, col] for col in row] for row in TFAOriginal]
+		TFVAQuality = [[[-1, -1] for col in row] for row in TFVARange]
 		for col in range(len(TFAOriginal[0])):
 			TFA = [[row[col]] for row in TFAOriginal]
 			singelGE = [[row[col]] for row in GE]
-			tfRange,meanTFActivities,algorithmResult = TFAHelper.TFVASingleCondition(CS,singelGE,TFA,iterLimit)
+			tfRange, meanTFActivities, algorithmResult = TFAHelper.TFVASingleCondition(CS, singelGE, TFA, iterLimit)
 
 			tempTFA = np.transpose(TFAVariable).tolist()
 			tempTFA[col] = meanTFActivities
 			TFAVariable = np.transpose(tempTFA).tolist()
 
-			tempTFVARange = np.transpose(TFVARange,(1, 0, 2)).tolist()
+			tempTFVARange = np.transpose(TFVARange, (1, 0, 2)).tolist()
 			tempTFVARange[col] = tfRange
-			TFVARange = np.transpose(tempTFVARange,(1, 0, 2)).tolist()
+			TFVARange = np.transpose(tempTFVARange, (1, 0, 2)).tolist()
 
-			tempTFVAQuality = np.transpose(TFVAQuality,(1, 0, 2)).tolist()
+			tempTFVAQuality = np.transpose(TFVAQuality, (1, 0, 2)).tolist()
 			tempTFVAQuality[col] = algorithmResult
-			TFVAQuality = np.transpose(tempTFVAQuality,(1, 0, 2)).tolist()
+			TFVAQuality = np.transpose(tempTFVAQuality, (1, 0, 2)).tolist()
 
 		if write:
-			TFAHelper.writeMatrix2csv(TFAVariable,open(outfile+".csv","w"))
-			pickle.dump(TFVARange,open(outfile+"Range.pkl","wb"))
-			pickle.dump(TFVAQuality,open(outfile+"Quality.pkl","wb"))
+			TFAHelper.writeMatrix2csv(TFAVariable, open(outfile + ".csv", "w"))
+			pickle.dump(TFVARange, open(outfile + "Range.pkl", "wb"))
+			pickle.dump(TFVAQuality, open(outfile + "Quality.pkl", "wb"))
 
+		return TFAVariable, TFVARange, TFVAQuality
 
-		return TFAVariable,TFVARange,TFVAQuality
-
-	@staticmethod
-	def TFVAMP(CS,GE,TFAOriginal,columns,q,iterLimit=20):
-		"""
-			Perform the variable TFA analysis on entire TFA matrix with complete GE matrix. The results for the
-			range of possible values as well as the difference in factorization qualtiy are written to pickle files
-			with the mean TFA activity written to a csv
-		"""
-
-		TFAVariable = list(TFAOriginal)
-		TFVARange = [[[col,col] for col in row] for row in TFAOriginal]
-		TFVAQuality = [[[-1,-1] for col in row] for row in TFVARange]
-		for col in range(len(TFAOriginal[0])):
-			TFA = [[row[col]] for row in TFAOriginal]
-			singelGE = [[row[col]] for row in GE]
-			tfRange,meanTFActivities,algorithmResult = TFAHelper.TFVASingleCondition(CS,singelGE,TFA,iterLimit)
-
-			tempTFA = np.transpose(TFAVariable).tolist()
-			tempTFA[col] = meanTFActivities
-			TFAVariable = np.transpose(tempTFA).tolist()
-
-			tempTFVARange = np.transpose(TFVARange,(1, 0, 2)).tolist()
-			tempTFVARange[col] = tfRange
-			TFVARange = np.transpose(tempTFVARange,(1, 0, 2)).tolist()
-
-			tempTFVAQuality = np.transpose(TFVAQuality,(1, 0, 2)).tolist()
-			tempTFVAQuality[col] = algorithmResult
-			TFVAQuality = np.transpose(tempTFVAQuality,(1, 0, 2)).tolist()
-
-
-		q.put([columns,TFAVariable,TFVARange,TFVAQuality])
-
-	@staticmethod
-	def TFVAMPRun(CS,GE,TFAOriginal,iterLimit=20,write=True,outfile = "TFVAResult",numCores = 2):
-		colsPerProc = int(len(GE[0])/numCores)
-		print(colsPerProc)
-		q = Queue()
-		startCol = 0
-		processes = []
-		TFAVariable = [[col for col in row] for row in TFAOriginal]
-		TFARange = [[[-1,-1] for col in row] for row in TFAOriginal]
-		TFAQuality = [[[-1,-1] for col in row] for row in TFAOriginal]
-
-		for p in range(numCores):
-			if p == numCores-1:
-				tempGE = [row[startCol:] for row in GE]
-				tempTFA = [row[startCol:] for row in TFAOriginal]
-				endCol = -1
-			else:
-				endCol = startCol+colsPerProc
-				tempGE = [row[startCol:endCol] for row in GE]
-				tempTFA = [row[startCol:endCol] for row in TFAOriginal]
-			p = Process(target = TFAHelper.TFVAMP,args=(CS,tempGE,tempTFA,[startCol,endCol],q))
-			p.start()
-			processes.append(p)
-			endCol = startCol
-		while any(x.is_alive() for x in processes): time.sleep(15)
-		while not q.empty():
-			[columns,TFAVar,TFVARan,TFVAQual] = q.get()
-			if columns[1] == -1:
-				TFAVariable = np.transpose(TFAVariable)
-				TFAVariable[columns[0]:] = np.transpose(TFAVar)
-				TFAVariable = np.transpose(TFAVariable).tolist()
-
-				TFARange = np.transpose(TFARange,(1, 0, 2)).tolist()
-				TFARange[columns[0]:] = np.transpose(TFVARan,(1,0,2))
-				TFARange = np.transpose(TFARange,(1, 0, 2)).tolist()
-
-				TFAQuality = np.transpose(TFAQuality,(1, 0, 2)).tolist()
-				TFAQuality[columns[0]:] = np.transpose(TFVAQual,(1,0,2))
-				TFAQuality = np.transpose(TFAQuality,(1, 0, 2)).tolist()
-			else:
-				TFAVariable = np.transpose(TFAVariable)
-				TFAVariable[columns[0]:columns[1]] = np.transpose(TFAVar)
-				TFAVariable = np.transpose(TFAVariable).tolist()
-
-				TFARange = np.transpose(TFARange,(1, 0, 2)).tolist()
-				TFARange[columns[0]:columns[1]] = np.transpose(TFVARan,(1,0,2))
-				TFARange = np.transpose(TFARange,(1, 0, 2)).tolist()
-
-				TFAQuality = np.transpose(TFAQuality,(1, 0, 2)).tolist()
-				TFAQuality[columns[0]:columns[1]] = np.transpose(TFVAQual,(1,0,2))
-				TFAQuality = np.transpose(TFAQuality,(1, 0, 2)).tolist()
-
-		if write:
-			TFAHelper.writeMatrix2csv(TFAVariable,open(outfile+".csv","w"))
-			pickle.dump(TFARange,open(outfile+"Range.pkl","wb"))
-			pickle.dump(TFAQuality,open(outfile+"Quality.pkl","wb"))
-
-		return TFAVariable,TFARange,TFAQuality
-
-
-
-
-		
 	@staticmethod
 	def rowv2colv(a):
 			"""
