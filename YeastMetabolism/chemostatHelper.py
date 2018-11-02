652f8a7ef06ed3a1995e79f65e5613bef548d8d4
estan <estancl1234@gmail.com>
2018-11-02 14:49:30 -0500

    fixed glucose model error. Still working on flux

--- YeastMetabolism/chemostatHelper.py
+++ YeastMetabolism/chemostatHelper.py
@@ -1,20 +1,23 @@
 import numpy as np
 import sys 
-from scipy import interpolate
+from scipy import interpolate,optimize,integrate
+import collections
+import matplotlib.pyplot as plt
 
 
 def OD2Biomass(od):
 	return 0.0160348 + 0.218138*od
 
 def convertCSVLineToFloats(line,delimiter):
-	return [float(val) for val in line.split(delimiter)]
+	return [float(val) for val in line.rstrip().split(delimiter)]
 
 def interpolateStandards(standardsFile,metabolites):
 	interpolationFunctions = {}
 	for meta in metabolites:
 		matrix =[convertCSVLineToFloats(line,",") for line 
-			in open(standardsFile+meta+".csv","r").readlines()]
-		matrix = [[0,0]]+[[row[0],np.mean(row[1:])] for row in matrix]	
+			in open(standardsFile+meta+".csv","rU").readlines()]
+		matrix = [[0,0]]+[[row[0],np.mean(row[1:])] for row in matrix]
+		if any(x<0 for x in [row[1] for row in matrix]): raise Exception("Bad Standard File")
 		interpolationFunctions[meta] = interpolate.interp1d([row[1] for row in matrix],
 			[row[0] for row in matrix],fill_value = "extrapolate")
 	return interpolationFunctions
@@ -24,18 +27,69 @@ def computeTNeg1Flux(flowRate,standardsInterpolation,hplcValue,biomass,mw):
 
 def interpolateVolume(volumeFile,delimiter):
 	matrix = [[float(x.rstrip().split(delimiter)[0]),float(x.rstrip().split(delimiter)[-1])]
-		for x in open(volumeFile,"r").readlines()[1:]]
+		for x in open(volumeFile,"rU").readlines()[1:]]
 	return interpolate.interp1d([row[0] for row in matrix],
 			[row[1] for row in matrix],fill_value = "extrapolate")
 
 def exponentialGrowthCurve(t,initialBMConc,c1):
-	return initialBMConc*(2**(c1*time))
+	if isinstance(t,collections.Iterable):
+		return [initialBMConc*(2**(c1*t1)) for t1 in t]
+	else:
+		return initialBMConc*(2**(c1*t))
 
-def fitExpGrowthModelFromBiomassConc(odMatrix):
-	time = [row[0] for row in odMatrixd]
-	medianODs = np.me
-	return scipy.optimize.curve_fit(exponentialGrowthCurve,time,biomass,
-		bounds = (0,[biomass[-1],1/60]))
+def fitExpGrowthModelFromBiomassConc(odMatrix,plot=False):
+	time = [row[0] for row in odMatrix]
+	medianBiomassConcs = [OD2Biomass(np.median([np.mean(row[1:4]),np.mean(row[4:7]),np.mean(row[7:])])) for row in odMatrix]
+	params, fit = optimize.curve_fit(exponentialGrowthCurve,time,medianBiomassConcs,
+		bounds = (0,[max(medianBiomassConcs),1.0/60]))
+	meanSError = mse(medianBiomassConcs,exponentialGrowthCurve(time,params[0],params[1]))
+	if plot:
+		plt.figure()
+		plt.scatter(time,medianBiomassConcs)
+		plt.xlabel("time")
+		plt.ylabel("Conc Biomass")
+		plt.title(str(meanSError))
+		plt.plot(range(int(max(time))),exponentialGrowthCurve(range(int(max(time))),params[0],params[1]))
 
+	return params,meanSError
 def exponentialGrowthRate(initialBMConc,c1,t):
 	return (2**(c1*t))*initialBMConc*c1*np.log(2)
+
+def glucoseConc(t,c0,cm,t0,initBiomass,c1):
+	glucoseConcIntegral = lambda t1: integrate.quad(glucoseConcIntegrand,0,t1,args=(c0,cm,t0,initBiomass,c1,))[0] 
+	if isinstance(t,collections.Iterable):
+		return [glucoseConcIntegral(t2) for t2 in t]
+	else:
+		return glucoseConcIntegral(t)
+
+def glucoseConcIntegrand(t,c0,cm,t0,initBiomass,c1):
+	return (c0*exponentialGrowthCurve(t,initBiomass,c1))/(1+2**(cm*(t0-t)))
+
+def fitGlucoseModel(c1,initBiomass,glucoseConcMatrix,interpFunc,plot=False):
+	time = [row[0] for row in glucoseConcMatrix]
+	T0Conc = interpFunc(np.mean(glucoseConcMatrix[0][1:]))
+
+	conc = [T0Conc - interpFunc(np.mean(row[1:])) for row in glucoseConcMatrix]
+	partiallyParamerterizedFunc = lambda t,c0,cm,t0:glucoseConc(t,c0,cm,t0,initBiomass,c1)
+	
+	params,fit = optimize.curve_fit(partiallyParamerterizedFunc,time,conc,bounds = ([0,0,-1000],[1,1000,1000]))
+	
+	meanSError = mse(conc,glucoseConc(time,params[0],params[1],params[2],initBiomass,c1))
+	if plot:
+		plt.figure()
+		plt.scatter(time,conc)
+		plt.xlabel("time")
+		plt.ylabel("Conc Glucose")
+		plt.title(str(meanSError))
+		plt.plot(range(int(max(time))),glucoseConc(range(int(max(time))),params[0],params[1],params[2],initBiomass,c1))
+
+	return params,meanSError
+
+def glucoseFlux(t,c0,cm,t0,initBiomass,c1,scale,mw):
+	return gramsGlucoseConsumptionRatePerUnitBiomass(t,c0,cm,t0,initBiomass,c1)*scale/mw
+
+def mse(measured,predicted):
+	return np.mean(np.power(np.subtract(measured,predicted),2))
+
+def gramsGlucoseConsumptionRatePerUnitBiomass(t,c0,cm,t0,initBiomass,c1):
+	return c0/(1+2**(cm*(t-t0)))
--- YeastMetabolism/generateSSEmpiricalFluxes.py
+++ YeastMetabolism/generateSSEmpiricalFluxes.py
@@ -1,78 +1,129 @@
 import sys 
 from chemostatHelper import *
+import matplotlib.pyplot as plt
 
-directory = sys.argv[1]
+print(glucoseConc(150,.03333,35.5151,-14.4463,.560,.00506))
+
+directoryBase = sys.argv[1]
 outputFile = sys.argv[2]
-strain = sys.argv[3]
+
+relevantMetabolites = ["aceticAcid","ethanol","glycerol","glucose"]
+molarMasses = [60.05,46.068,92.0938,180.156]
 accuracy = 5
+timeScale = 60
+biomassVolume = .03
+timePointsOfInterestInBolus = [30,90,300]
 
-relevantMetabolites = ["aceticAcid","ethanol","glycerol"]
-
-molarMasses = [60.05,46.068,92.0938]
-
-writeHeaders = False
-
-try:
-	checkOutput = open(outputFile,"r")
-	checkOutput.close()
-	outputFile = open(outputFile,"a")
-except:
-	outputFile = open(outputFile,"w")
-	outputFile.write("strain,")
-	for meta in relevantMetabolites:
-		outputFile.write(meta+",")
-	outputFile.write("growthRate")
 
 hplcStandards = "hplcStandards/"
+odFile = "od.csv"
+volumeFile = "volume.csv"
+mediaFlowRateFile = "mediaFlowRate.csv"
+dryWeightFile = "dryWeight.csv"
+timeT0ToTNeg1File = "timeTNeg1ToT0.csv"
 
 
+metadata =  [x.rstrip().split(",") for x in open(directoryBase+"metadata.csv","rU").readlines()]
+headers = metadata[0]
+metadata = metadata[1:-1]
+
+headerToIndex = {header:pos for header,pos in zip(headers,range(len(headers)))}
+
+outputFile = open(outputFile,"w")
+outputFile.write("strain,")
+for meta in relevantMetabolites[:4]:
+	outputFile.write("TNeg1Flux"+meta+",")
+outputFile.write("biomassTNeg1"+",")
+outputFile.write("growthRateTNeg1"+",")
+outputFile.write("ExponentialGrowthModelMSE,")
+outputFile.write("glucoseModelFit,")
+[outputFile.write("glucoseFlux"+str(time)+",") for time in timePointsOfInterestInBolus]
+
+	
 metaboliteMolecularWeights = {meta:val for meta,val in zip(relevantMetabolites,
 	molarMasses)}
 
-odFile = "od.csv"
+badFileBeyondJustNE = 0
+goodFiles = 0
 
-try:
-	interpFunction = interpolateStandards(directory+hplcStandards,relevantMetabolites)
+for sample in metadata:
+	directory = directoryBase+sample[-1]+"/"
+	strain = sample[headerToIndex["strainID"]]+"_"+sample[headerToIndex["date"]]+"_"+sample[headerToIndex["chemostat"]]
+	if sample[headerToIndex["goodOD"]] == "yes" and sample[headerToIndex["goodDW"]] == "yes" and sample[headerToIndex["goodHPLC"]] == "yes":
+		goodFiles+=1
+		try:
+			if goodFiles < 10:
+				plot = False
+			else:
+				plot = False
 
-	timeScale = 60
-	flowRate = float(open(directory+"mediaFlowRate.csv","r").readline())*timeScale
+			interpFunction = interpolateStandards(directory+hplcStandards,relevantMetabolites)
 
-	odFile = open(directory+odFile,"r")
+			flowRate = float(open(directory+mediaFlowRateFile,"rU").readline())*timeScale
 
-	# od = convertCSVLineToFloats(odFile.readline(),",")[1:]
-	# od = np.median([np.mean(od[0:3]),np.mean(od[3:6]),np.mean(od[6:])])
+			ods = open(directory+odFile,"rU")
+			ods.readline()
 
-	volumeInterpolator = interpolateVolume(directory+"volume.csv",",")
+			volumeInterpolator = interpolateVolume(directory+volumeFile,",")
+			biomassConc = np.median(convertCSVLineToFloats(open(directory+dryWeightFile,"rU").readline(),",")[1:])/biomassVolume
+			biomass = biomassConc*volumeInterpolator(-1)
 
-	#biomass = OD2Biomass(od)*volumeInterpolator(-1)
+			metaFluxValues = []
+			metaFiles = {}
+			for meta in relevantMetabolites[:4]:
+				metaFile = open(directory+meta+".csv","rU")
+				metaFluxValues.append(np.round(np.mean(
+					[computeTNeg1Flux(flowRate,interpFunction[meta],val,biomass,metaboliteMolecularWeights[meta]) for val
+					in convertCSVLineToFloats(metaFile.readline(),",")[1:]]),accuracy))
+				metaFiles[meta] = metaFile
 
-	biomassVolume = .03
-	biomassConc = np.mean(convertCSVLineToFloats(open(directory+"dryWeight.csv","r").readline(),",")[1:])/biomassVolume
-	biomass = biomassConc*volumeInterpolator(-1)
 
-	metaFluxValues = []
-	for meta in relevantMetabolites:
-		metaFluxValues.append(np.round(np.mean(
-			[computeTNeg1Flux(flowRate,interpFunction[meta],val,biomass,metaboliteMolecularWeights[meta]) for val
-			in convertCSVLineToFloats(open(directory+meta+".csv","r").readline(),",")[1:]]),accuracy))
+			outputFile.write("\n"+strain)
+			for meta in metaFluxValues: outputFile.write(","+str(meta))
+			outputFile.write(","+str(biomass
+				))
+			params,fit = fitExpGrowthModelFromBiomassConc([convertCSVLineToFloats(line,",") for line in ods.readlines()],plot=plot)
 
-	odT0 = convertCSVLineToFloats(odFile.readline(),",")[1:]
-	odT0 = np.median([np.mean(odT0[0:3]),np.mean(odT0[3:6]),np.mean(odT0[6:])])
-	biomassT0 = OD2Biomass(odT0)
+			biomassT0 = params[0]
 
-	timeT0ToTNeg1 = -1*float(open(directory+"timeTNeg1ToT0.csv","r").readline().rstrip().split(",")[0])
-	growthRate = (biomassT0-biomassConc)*volumeInterpolator(-1)*(timeScale/timeT0ToTNeg1)/biomass
+			c1 = params[1]
+
+			timeT0ToTNeg1 = abs(float(open(directory+timeT0ToTNeg1File,"rU").readline().rstrip()))
+			
+			volume = volumeInterpolator(-1)
+
+			#growthRate = (biomassT0-biomassConc)*(volume)*(timeScale/timeT0ToTNeg1)/biomass
+			
+			growthRate = exponentialGrowthRate(params[0],params[1],0)*timeScale/params[0]
+
+			outputFile.write(","+str(np.round(growthRate,accuracy))+","+str(fit))
+
+			#compute glucose model fit
 	
-	outputFile.write("\n"+strain)
-	for meta in metaFluxValues: outputFile.write(","+str(meta))
-	outputFile.write(","+str(np.round(growthRate,accuracy)))
-    
-except OSError as e:
-	pass
-except:
-	print("error")
+			params,fit = fitGlucoseModel(params[1],params[0],[convertCSVLineToFloats(line,",") for line in open(directory+"glucose.csv","rU").readlines()[1:]],interpFunc = interpFunction["glucose"],plot=plot)
+
+			outputFile.write(","+str(fit))
+
+			[outputFile.write(","+str(flux)) for flux in [glucoseFlux(t,params[0],params[1],params[2],biomassT0,c1,timeScale,metaboliteMolecularWeights["glucose"]) for t in timePointsOfInterestInBolus]]
 
 
+
+
+		
+		except (OSError,IOError):
+			#print(directory, "missing files")
+			pass
+		except Exception as inst:
+			#print(directory, inst.args[0])
+			badFileBeyondJustNE+=1
+			pass
+		except:
+			#print(directory, sys.exc_info()[0])
+			badFileBeyondJustNE+=1
+
+print(1-badFileBeyondJustNE/float(goodFiles))
+
+plt.show()
 
 
 
